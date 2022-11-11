feddccdf291be9fcb99c0e219a0e705edc637f77
kharileigh <kharileighmiller@gmail.com>
2022-11-11 10:54:29 +0000

    BOOK REFACTORING : Component Annotation

--- BookSpringFrameworkAnnotationRefactoring/src/main/java/com/kharileigh/bookannotationrefactoring/client/BookConfiguration.java
+++ BookSpringFrameworkAnnotationRefactoring/src/main/java/com/kharileigh/bookannotationrefactoring/client/BookConfiguration.java
@@ -6,10 +6,6 @@
 
 package com.kharileigh.bookannotationrefactoring.client;
 
-import com.kharileigh.bookannotationrefactoring.persistence.BookDaoImpl;
-import com.kharileigh.bookannotationrefactoring.presentation.BookPresentationImpl;
-import com.kharileigh.bookannotationrefactoring.service.BookServiceImpl;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 

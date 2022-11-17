0736b8385092ec8ec5f052d3a2fae2cdc63cc54f
kharileigh <kharileighmiller@gmail.com>
2022-11-17 16:11:57 +0000

    BookSpringFrameworkProject : REFACTORED DATABASE USING JDBC & SQL

---
+++ employeeUpdate.sql
@@ -0,0 +1,16 @@
+use wileyDI001;
+
+select * from employee;
+
+alter table employee
+add doj date;
+
+set SQL_SAFE_UPDATES=0;
+
+update employee
+set doj = "2018-12-11"
+where department="Sales";
+
+update employee
+set doj = "2021-05-28"
+where department="Marketing";
